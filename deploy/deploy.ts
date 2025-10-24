163abb225358179b838e119ef31bc85aa418ee9a
danielwardx <EdgmanSchepper12091988gjr@hotmail.com>
2025-10-24 14:44:51 +0800

    update deploy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedTravelRegistry = await deploy("TravelRegistry", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`TravelRegistry contract: `, deployedTravelRegistry.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_travelRegistry"; // id required to prevent reexecution
+func.tags = ["TravelRegistry"];
